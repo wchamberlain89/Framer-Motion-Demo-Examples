fadab2bed10c7a7e481287ddeded30a36b018428
wc <wchamberlain89@gmail.com>
2021-09-08 11:35:27 -0700

    Adds ExampleSix Fade in up with intersection observer

--- src/ExampleSix/index.jsx
+++ src/ExampleSix/index.jsx
@@ -1,9 +1,64 @@
 import React from 'react';
+import { useInView } from 'react-intersection-observer'
+import { motion } from 'framer-motion'
+
+const List = ({ data }) => {
+  const { ref, inView } = useInView();
+
+  const listVariants = {
+    hidden: {
+      opacity: 0
+    },
+    showing: {
+      opacity: 1,
+      transition: {
+        duration: 1,
+        staggerChildren: 0.08
+      }
+    }
+  }
+
+  const listItemVariants = {
+    showing: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.35
+      }
+    },
+    hidden: {
+      opacity: 0,
+      y: 40
+    }
+  }
+
+  return (
+    <motion.ul initial='hidden' animate={`${inView ? 'showing' : ''}`} variants={listVariants} ref={ref}>
+      { data.map( item => <Item variants={listItemVariants} id={ item + 1 } key={item}/>) }
+    </motion.ul>
+  )
+}
+
+const Item = ({ variants, id }) => {
+  return (
+    <motion.li variants={variants} className='square'>
+      <div>
+        {  id  }
+      </div>
+    </motion.li>
+  )
+}
+
+
 
 const ExampleSix = () => {
+  const dataArraySize = 30
+  const data = Array.apply(null, Array(dataArraySize)).map(function (x, i) { return i; })
+  
   return (
-    <div>
-      
+    <div className='example-container' id='example-five'>
+      <h2>Example Six</h2>
+      <List data={data}/>
     </div>
   );
 };
